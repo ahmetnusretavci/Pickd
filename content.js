a920441dd4843158e5a1f7117fb5969ee43bfb03
Nusret <ahmetnusretavci34@gmail.com>
2026-02-23 21:51:31 +0300

    Smooth scrolling implemented for bypassing lazy loading

--- content.js
+++ content.js
@@ -9,19 +9,64 @@ function getPosterUrl(img) {
   return url.replace(/-0-\d+-0-\d+-crop/, '-0-500-0-750-crop');
 }
 
+let hasAutoScrolledForPick = false;
+
+function autoScrollThen(callback) {
+  const scrollTargetBottom = Math.max(
+    document.documentElement.scrollHeight,
+    document.body.scrollHeight
+  );
+
+  const scrollOptions = (top) => ({
+    top,
+    behavior: 'smooth'
+  });
+
+  // Scroll to bottom
+  window.scrollTo(scrollOptions(scrollTargetBottom));
+
+  const singleScrollDuration = 1000;
+
+  setTimeout(() => {
+    window.scrollTo(scrollOptions(0));
+
+    setTimeout(() => {
+      callback();
+    }, singleScrollDuration);
+  }, singleScrollDuration);
+}
+
+function pickRandomFilm() {
+  const movies = document.querySelectorAll('li.griditem .react-component');
+  const films = Array.from(movies).map((movie) => {
+    const poster = getPosterUrl(movie.querySelector('img'));
+    return {
+      name: movie.dataset.itemName,
+      poster,
+      link: movie.dataset.targetLink
+    };
+  });
+
+  if (!films.length) {
+    return null;
+  }
+
+  return films[Math.floor(Math.random() * films.length)];
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'pick') {
-    const movies = document.querySelectorAll('li.griditem .react-component');
-    const films = Array.from(movies).map(movie => {
-      const poster = getPosterUrl(movie.querySelector('img'));
-      return {
-        name: movie.dataset.itemName,
-        poster,
-        link: movie.dataset.targetLink
-      };
-    });
-    const randomFilm = films[Math.floor(Math.random() * films.length)];
-    sendResponse({ film: randomFilm });
+    const respondWithRandomFilm = () => {
+      const randomFilm = pickRandomFilm();
+      sendResponse({ film: randomFilm });
+    };
+
+    if (!hasAutoScrolledForPick) {
+      hasAutoScrolledForPick = true;
+      autoScrollThen(respondWithRandomFilm);
+    } else {
+      respondWithRandomFilm();
+    }
   }
   return true;
 });
